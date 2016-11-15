9fda13018382b8a82c74548687b6dfbfb26ce910
Kenan AGKURT <kenanagkurt@gmail.com>
2016-11-15 05:31:03 +0200

    Update tr-TR.php

--- upload/converter/language/tr-TR/tr-TR.php
+++ upload/converter/language/tr-TR/tr-TR.php
@@ -10,9 +10,9 @@ $_['text_on']			= 'AÇIK';
 $_['text_simulation']	        = 'Simülasyon';
 $_['text_step']		= 'Adım %s/%s';
 // Btn
-$_['btn_back']  	    = 'Back';
-$_['btn_clean']	        = 'Clean settings';
-$_['btn_clean_dbase']   = 'Drop';
+$_['btn_back']  	    = 'Geri';
+$_['btn_clean']	        = 'Ayarları Temizle';
+$_['btn_clean_dbase']   = 'Boşalt';
 $_['btn_collate']       = 'Update collate';
 $_['btn_column']        = 'Add columns';
 $_['btn_config']		= 'Yapılandırma dosyalarını güncelleştir';
@@ -25,24 +25,24 @@ $_['btn_login']   = 'Oturum Açma';
 $_['btn_images']        = 'Upgrade image paths';
 $_['btn_info']          = 'Database information';
 $_['btn_logout']      = 'Logout';
-$_['btn_module']        = 'Move modules';
+$_['btn_module']        = 'Modülleri Taşı';
 $_['btn_permission']  = 'Yönetici izinleri';
-$_['btn_preview']	    = 'Continue to preview status';
+$_['btn_preview']	    = 'Önizleme ile devam et';
 $_['btn_save']        = 'Kaydet';
-$_['btn_setting']       = 'Add settings';
+$_['btn_setting']       = 'Ayarlar Ekle';
 $_['btn_skip']	    	= 'Atla';
 $_['btn_start']			= 'Güncelleştirmeyi Başlat';
 $_['btn_upgrade']     = 'Güncelleştirmeyi Başlat';
 
 // Step
-$_['step_start']         = 'Add new tables';
+$_['step_start']         = 'Yeni tablo ekle';
 $_['step_collate']       = 'Database collation';
 $_['step_column']        = 'Add new columns';
 $_['step_data']          = 'Add new data';
 $_['step_module']        = 'Move modules';
 $_['step_setting']       = 'Add new settings';
-$_['step_configuration'] = 'Config.php files';
-$_['step_images']        = 'Change image paths';
-$_['step_clean_module']  = 'Clean settings';
+$_['step_configuration'] = 'Config.php dosyaları';
+$_['step_images']        = 'Resim yolunu değiştir';
+$_['step_clean_module']  = 'Ayarları temizle';
 $_['step_clean_table']   = 'Drop expired structure';
 ?>
